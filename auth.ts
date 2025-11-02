c80af7fb8cc4bf23e7079c8150dc0d4d83d70fec
Dastan Ozgeldi <ozgdastan@gmail.com>
2025-11-02 19:19:45 +0900

    refactor

--- auth.ts
+++ auth.ts
@@ -1,10 +1,14 @@
 import { betterAuth } from "better-auth";
 import { drizzleAdapter } from "better-auth/adapters/drizzle";
 import db from "./drizzle/db";
+import { nextCookies } from "better-auth/next-js";
 
 export const auth = betterAuth({
-  database: drizzleAdapter(db, { provider: "pg" }),
   emailAndPassword: {
     enabled: true,
   },
+  database: drizzleAdapter(db, {
+    provider: "pg",
+  }),
+  plugins: [nextCookies()],
 });
