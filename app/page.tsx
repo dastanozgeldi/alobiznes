4fc7f1094c70c506d91025ff14107880d54b1517
Dastan Ozgeldi <ozgdastan@gmail.com>
2025-11-02 17:39:10 +0900

    feat: display in table

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,26 @@
 
 import { useActionState } from "react";
 import { readReceipt } from "./actions";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+import { Label } from "@/components/ui/label";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { Skeleton } from "@/components/ui/skeleton";
+import { Loader2Icon } from "lucide-react";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
 
 const initialState = {
   success: false,
@@ -15,21 +35,97 @@ export default function Page() {
   );
 
   return (
-    <div className="flex flex-col gap-4 min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <h1 className="text-2xl font-bold">Ало, бизнес?</h1>
-      <form action={formAction}>
-        <div className="flex flex-col gap-2">
-          <label htmlFor="receipt">Загрузить чек</label>
-          <input type="file" name="receipt" id="receipt" accept="image/*" />
+    <div className="flex flex-col min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+      <div className="max-w-2xl mx-auto flex flex-col gap-4 w-full">
+        <div>
+          <h1 className="text-2xl font-bold">Ало, бизнес?</h1>
+          <p className="text-muted-foreground font-medium">
+            Финансовая грамотность с помощью одной фотки чека.
+          </p>
         </div>
-        <button className="mt-6">Submit</button>
-      </form>
 
-      {isPending && <p>Loading...</p>}
+        <form action={formAction} className="space-y-4">
+          <div className="flex items-center gap-3">
+            <div className="flex flex-col gap-1.5 w-full">
+              <Label>Ориг. валюта</Label>
+              <Select name="original_currency" required defaultValue="JPY">
+                <SelectTrigger className="w-full">
+                  <SelectValue placeholder="Выберите валюту" />
+                </SelectTrigger>
+                <SelectContent>
+                  <SelectItem value="JPY">Японский йен</SelectItem>
+                  <SelectItem value="USD">Американский доллар</SelectItem>
+                  <SelectItem value="EUR">Евро</SelectItem>
+                  <SelectItem value="KZT">Казахстанский тенге</SelectItem>
+                </SelectContent>
+              </Select>
+            </div>
 
-      {state.success && (
-        <pre className="mt-6">{JSON.stringify(state.data, null, 2)}</pre>
-      )}
+            <div className="flex flex-col gap-1.5 w-full">
+              <Label>Конвертированная валюта</Label>
+              <Select name="converted_currency" required defaultValue="KZT">
+                <SelectTrigger className="w-full">
+                  <SelectValue placeholder="Выберите валюту" />
+                </SelectTrigger>
+                <SelectContent>
+                  <SelectItem value="JPY">Японский йен</SelectItem>
+                  <SelectItem value="USD">Американский доллар</SelectItem>
+                  <SelectItem value="EUR">Евро</SelectItem>
+                  <SelectItem value="KZT">Казахстанский тенге</SelectItem>
+                </SelectContent>
+              </Select>
+            </div>
+          </div>
+
+          <div className="flex flex-col gap-1.5">
+            <Label htmlFor="receipt">Загрузить чек (до 3MB)</Label>
+            <Input
+              type="file"
+              name="receipt"
+              id="receipt"
+              accept="image/*"
+              required
+            />
+          </div>
+
+          <Button disabled={isPending}>
+            {isPending ? (
+              <>
+                <Loader2Icon className="size-4 animate-spin" />
+                Обрабатываем
+              </>
+            ) : (
+              "Отправить"
+            )}
+          </Button>
+        </form>
+
+        {isPending && <Skeleton className="h-[400px] w-full" />}
+        {state.success && state.data && (
+          <Table>
+            <TableHeader>
+              <TableRow className="bg-muted">
+                <TableHead>Дата</TableHead>
+                <TableHead>Название</TableHead>
+                <TableHead>Цена</TableHead>
+                <TableHead>Конвертированная цена</TableHead>
+                <TableHead>Перевод названия</TableHead>
+              </TableRow>
+            </TableHeader>
+            <TableBody>
+              {state.data.map((item, index) => (
+                <TableRow key={index}>
+                  <TableCell>{item.date}</TableCell>
+                  <TableCell>{item.name}</TableCell>
+                  <TableCell>{item.original_price}</TableCell>
+                  <TableCell>{item.converted_price}</TableCell>
+                  <TableCell>{item.name_translation}</TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        )}
+      </div>
     </div>
   );
 }
